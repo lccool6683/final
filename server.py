c1744200bd3b7ce92f9a93e9c54d2fcde6e6d498
root <kylegilles@hotmail.com>
2016-12-03 15:05:37 -0800

    aadded config

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
-#
+#server / attacker
+import ConfigParser
+
+configParser = ConfigParser.RawConfigParser()
+configFilePath = r'config.txt'
+configParser.read(configFilePath)
+
+dstIP = configParser.get('config', 'dstIP')
+srcIP = configParser.get('config', 'srcIP')
+dstPort = configParser.get('config', 'dstPort')
+fileDir = configParser.get('config', 'fileDir')
+
+print dstIP
