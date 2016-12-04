61807c968dd5e5ba9ff0d8f4f57b93018de784a6
root <kylegilles@hotmail.com>
2016-12-03 16:41:09 -0800

    command thread

--- server.py
+++ server.py
@@ -11,3 +11,41 @@ dstPort = configParser.get('config', 'dstPort')
 fileDir = configParser.get('config', 'fileDir')
 
 print dstIP
+
+
+
+def getCmd(dstIP, srcIP, dstPort):
+	while True: 
+		cmd = raw_input("Enter a command: ")
+
+		if cmd =="exit":
+			print "Exiting"
+			sys.exit()
+
+
+		elif cmd =="close":
+			#drop iptables rule
+			print "Closing port"
+
+		else :
+			#create a packet to send to the victim
+			print "your command " + cmd
+
+
+
+
+
+
+
+
+
+
+#2 main threads. User commands & file extraction
+def main():
+
+	cmdThread = threading.Thread(target=getCmd, args=(dstIP, srcIP, dstPort))
+	#fileThread = threading.Thread(target=getFile, args=(dstIP, srcIP, dstPort))
+	
+
+if __name__== '__main__':
+	main()
